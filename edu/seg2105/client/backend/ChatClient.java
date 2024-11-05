df0a8b1c0d36c8cabb8b43bd3b7810975944fad4
Miranda Deng <mdeng027@uottawa.ca>
2024-11-05 17:22:32 -0500

    Reformatting tabs

--- edu/seg2105/client/backend/ChatClient.java
+++ edu/seg2105/client/backend/ChatClient.java
@@ -18,179 +18,169 @@ import edu.seg2105.client.common.*;
  * @author Dr Robert Lagani&egrave;
  * @author Fran&ccedil;ois B&eacute;langer
  */
-public class ChatClient extends AbstractClient
-{
-  //Instance variables **********************************************
-  
-  /**
-   * The interface type variable.  It allows the implementation of 
-   * the display method in the client.
-   */
-  ChatIF clientUI; 
+public class ChatClient extends AbstractClient {
+    //Instance variables **********************************************
 
-  
-  //Constructors ****************************************************
-  
-  /**
-   * Constructs an instance of the chat client.
-   *
-   * @param host The server to connect to.
-   * @param port The port number to connect on.
-   * @param clientUI The interface type variable.
-   */
-  
-  public ChatClient(String host, int port, ChatIF clientUI) 
-    throws IOException 
-  {
-    super(host, port); //Call the superclass constructor
-    this.clientUI = clientUI;
-    openConnection();
-  }
+    /**
+     * The interface type variable.  It allows the implementation of
+     * the display method in the client.
+     */
+    ChatIF clientUI;
 
-  
-  //Instance methods ************************************************
-    
-  /**
-   * This method handles all data that comes in from the server.
-   *
-   * @param msg The message from the server.
-   */
-  public void handleMessageFromServer(Object msg) 
-  {
-    clientUI.display(msg.toString());
-    
-    
-  }
 
-  /**
-   * This method handles all data coming from the UI            
-   *
-   * @param message The message from the UI.    
-   */
-  public void handleMessageFromClientUI(String message)
-  {
-    try
-    {
-      if (message.startsWith("#")) {
-        handleCommand(message);
-      }
-      else {
-        sendToServer(message);
-      }
+    //Constructors ****************************************************
+
+    /**
+     * Constructs an instance of the chat client.
+     *
+     * @param host     The server to connect to.
+     * @param port     The port number to connect on.
+     * @param clientUI The interface type variable.
+     */
+
+    public ChatClient(String host, int port, ChatIF clientUI)
+            throws IOException {
+        super(host, port); //Call the superclass constructor
+        this.clientUI = clientUI;
+        openConnection();
     }
-    catch(IOException e)
-    {
-      clientUI.display
-        ("Could not send message to server.  Terminating client.");
-      quit();
+
+
+    //Instance methods ************************************************
+
+    /**
+     * This method handles all data that comes in from the server.
+     *
+     * @param msg The message from the server.
+     */
+    public void handleMessageFromServer(Object msg) {
+        clientUI.display(msg.toString());
+
+
     }
-  }
 
-  private void handleCommand(String message) {
-      String[] args = message.split(" ");
-      String command = args[0];
-      switch (command) {
-        case "#quit":
-          quit();
-          break;
+    /**
+     * This method handles all data coming from the UI
+     *
+     * @param message The message from the UI.
+     */
+    public void handleMessageFromClientUI(String message) {
+        try {
+            if (message.startsWith("#")) {
+                handleCommand(message);
+            } else {
+                sendToServer(message);
+            }
+        } catch (IOException e) {
+            clientUI.display
+                    ("Could not send message to server.  Terminating client.");
+            quit();
+        }
+    }
 
-        case "#logoff":
-          try {
+    private void handleCommand(String message) {
+        String[] args = message.split(" ");
+        String command = args[0];
+        switch (command) {
+            case "#quit":
+                quit();
+                break;
+
+            case "#logoff":
+                try {
+                    closeConnection();
+                } catch (IOException e) {
+                    clientUI.display("ERROR - Could not close connection!");
+                }
+                break;
+
+            case "#login":
+                if (this.isConnected()) {
+                    clientUI.display("Client is already connected.");
+                } else {
+                    try {
+                        this.openConnection();
+                        connectionOpen();
+                    } catch (IOException e) {
+                        clientUI.display("ERROR - Could not open connection!");
+                    }
+                }
+                break;
+
+            case "#sethost":
+                if (this.isConnected()) {
+                    clientUI.display("Cannot set host if client is still connected.");
+                } else if (args.length > 1) {
+                    this.setHost(args[1]);
+                    System.out.println("Host set to " + getHost());
+                }
+                break;
+
+            case "#setport":
+                if (this.isConnected()) {
+                    clientUI.display("Cannot set port if client is still connected.");
+                } else if (args.length > 1) {
+                    try {
+                        this.setPort(Integer.parseInt(args[1]));
+                        System.out.println("Port set to " + getPort());
+                    } catch (NumberFormatException e) {
+                        clientUI.display("Invalid port number. Please provide a valid integer.");
+                    }
+                }
+                break;
+
+            case "#gethost":
+                clientUI.display("Current host is " + this.getHost());
+                break;
+
+            case "#getport":
+                clientUI.display("Current port is " + this.getPort());
+                break;
+
+            default:
+                clientUI.display("Invalid command: '" + command + "'");
+                break;
+        }
+    }
+
+    /**
+     * This method terminates the client.
+     */
+    public void quit() {
+        try {
             closeConnection();
-          } catch (IOException e) {
-            clientUI.display("ERROR - Could not close connection!");
-          }
-          break;
-
-        case "#login":
-          if (this.isConnected()) {
-            clientUI.display("Client is already connected.");
-          } else {
-            try {
-              this.openConnection();
-              connectionOpen();
-            } catch (IOException e) {
-              clientUI.display("ERROR - Could not open connection!");
-            }
-          }
-          break;
-
-        case "#sethost":
-          if (this.isConnected()) {
-            clientUI.display("Cannot set host if client is still connected.");
-          } else if (args.length > 1) {
-            this.setHost(args[1]);
-            System.out.println("Host set to " + getHost());
-          }
-          break;
-
-        case "#setport":
-          if (this.isConnected()) {
-            clientUI.display("Cannot set port if client is still connected.");
-          } else if (args.length > 1) {
-            try {
-              this.setPort(Integer.parseInt(args[1]));
-              System.out.println("Port set to " + getPort());
-            } catch (NumberFormatException e) {
-              clientUI.display("Invalid port number. Please provide a valid integer.");
-            }
-          }
-          break;
-
-        case "#gethost":
-          clientUI.display("Current host is " + this.getHost());
-          break;
-
-        case "#getport":
-          clientUI.display("Current port is " + this.getPort());
-          break;
-
-        default:
-          clientUI.display("Invalid command: '" + command + "'");
-          break;
-      }
-  }
-
-  /**
-   * This method terminates the client.
-   */
-  public void quit()
-  {
-    try
-    {
-      closeConnection();
+        } catch (IOException e) {
+            System.out.println("ERROR - Could not close connection!");
+        }
+        System.exit(0);
     }
-    catch(IOException e) {
-      System.out.println("ERROR - Could not close connection!");
+
+    /**
+     * Implements the hook method called each time an exception is thrown by the client's
+     * thread that is waiting for messages from the server. The method may be
+     * overridden by subclasses.
+     *
+     * @param exception the exception raised.
+     */
+    @Override
+    protected void connectionException(Exception exception) {
+        clientUI.display("The server has shut down");
+        System.exit(0);
     }
-    System.exit(0);
-  }
 
-  /**
-   * Implements the hook method called each time an exception is thrown by the client's
-   * thread that is waiting for messages from the server. The method may be
-   * overridden by subclasses.
-   *
-   * @param exception the exception raised.
-   */
-  @Override
-  protected void connectionException(Exception exception) {
-    clientUI.display("The server has shut down");
-    System.exit(0);
-  }
-  /**
-   * Implemented the hook method called after the connection has been closed. The default
-   * implementation does nothing. The method may be overriden by subclasses to
-   * perform special processing such as cleaning up and terminating, or
-   * attempting to reconnect.
-   */
-  @Override
-  protected void connectionClosed() {
-    clientUI.display("Connection closed");
-  }
+    /**
+     * Implemented the hook method called after the connection has been closed. The default
+     * implementation does nothing. The method may be overriden by subclasses to
+     * perform special processing such as cleaning up and terminating, or
+     * attempting to reconnect.
+     */
+    @Override
+    protected void connectionClosed() {
+        clientUI.display("Connection closed");
+    }
 
-  protected void connectionOpen() {
-    clientUI.display("Connection open");
-  }
+    protected void connectionOpen() {
+        clientUI.display("Connection open");
+    }
 }
 //End of ChatClient class
--- edu/seg2105/client/ui/ServerConsole.java
+++ edu/seg2105/client/ui/ServerConsole.java
@@ -46,29 +46,22 @@ public class ServerConsole implements ChatIF {
      * the server instance (there is no UI in this phase).
      *
      * @param args\[0] The port number to listen on.  Defaults to 5555
-     *          if no argument is entered.
+     *                 if no argument is entered.
      */
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         int port = 0; //Port to listen on
 
-        try
-        {
+        try {
             port = Integer.parseInt(args[0]); //Get port from command line
-        }
-        catch(Throwable t)
-        {
+        } catch (Throwable t) {
             port = DEFAULT_PORT; //Set port to 5555
         }
 
         EchoServer sv = new EchoServer(port);
 
-        try
-        {
+        try {
             sv.listen(); //Start listening for connections
-        }
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
             System.out.println("ERROR - Could not listen for clients!");
         }
     }
